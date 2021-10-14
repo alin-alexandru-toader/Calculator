4123ae0721428258e485bb426929293904f84051
Alex <alextoader8@gmail.com>
2021-10-14 12:38:34 +0300

    Finished working on buttons

--- Calculator/myscript.js
+++ Calculator/myscript.js
@@ -1,12 +1,8 @@
 
 let add = (a, b) => Number(a) + Number(b);
-
 let subtract = (a, b) => Number(a) - Number(b);
-
 let multiply = (a, b) => Number(a) * Number(b);
-
 let divide = (a, b) => Number(a) / Number(b);
-
 
 function operate(num1, operator, num2) {
     if (operator === "+") {
@@ -22,3 +18,56 @@ function operate(num1, operator, num2) {
         return divide(num1, num2);
     }
 }
+
+
+
+const buttons = document.getElementsByTagName('button');
+for(let i=0; i<buttons.length; i++) {
+    buttons[i].onclick = function() {
+        if (buttons[i].className == "digit") {
+            let input = document.getElementById('input');
+            input.textContent += this.textContent;    
+        }
+
+        else if(buttons[i].className == "bkspc") {
+            let input = document.getElementById('input');
+            input.textContent = input.textContent.slice(0, -1);         
+        }
+    
+        else if(buttons[i].className == "float") {             
+            let input = document.getElementById('input');
+            input.textContent += this.textContent;
+        }
+        else if(buttons[i].className == "clear") {
+            let input = document.getElementById('input');
+            input.textContent = input.textContent.slice(0, 0);        
+        }
+
+        else if (buttons[i].className == "operator") {
+            let input = document.getElementById('input');
+            input.textContent += this.textContent;    
+        }
+
+        else if (buttons[i].className == "equality") {
+            let input = document.getElementById('input');
+    
+            let toBeSolved = input.textContent;        
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
